a05db8c056589522e774a98f18dcb93640800e9c
Aiste Laugayte <aiste.laugalyte@gmail.com>
2020-04-21 17:15:42 +0300

    Clean up warnings

--- app/src/main/java/com/example/mydinner/LoginActivity.java
+++ app/src/main/java/com/example/mydinner/LoginActivity.java
@@ -9,9 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class LoginActivity extends AppCompatActivity {
 
 
